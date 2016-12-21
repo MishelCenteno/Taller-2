83d29e3ece14640fd9219aa2b5843b5fd6d29878
Usuario01-PC\Usuario01 <mishelcenteno96@gmail.com>
2016-12-21 13:06:38 -0500

    Ejercicios en clase

---
+++ EjerciciosEnClase.py
@@ -0,0 +1,78 @@
+import turtle
+t=turtle.Pen()
+
+##t.forward(50)
+##t.left(90)
+##t.forward(50)
+##t.left(90)
+##t.forward(50)
+##t.left(90)
+##t.forward(50)
+##turtle.getscreen()._root.mainloop()
+
+##t.reset()
+##for x in range(1,6):
+##    t.forward(100)
+##    t.left(45)
+
+##t.rest()
+##n=int(input("ingrese un numero impar: "))
+##angulo=180/n
+##if n%2==0:
+##    for x in range (0,n):
+##        t.forward(100)
+##        t.left(angulo*2)
+##else:
+##    for x in range (0,n):
+##        t.forward(100)
+##        t.left(angulo*4)
+##    
+##
+##def micuadrado(size):
+##    for x in range(1,5):
+##        t.forward(size)
+##        t.left(90)
+##        
+##micuadrado(90)
+
+def nombre():
+    t=turtle.Pen()
+    t.left(135)
+    t.forward(50)
+    
+    t.left(225)
+    t.forward(70)
+    
+    t.left(270)
+    t.forward(150)
+    
+    t.left(270)
+    t.forward(100)
+    
+    t.left(270)
+    t.forward(50)
+    
+    t.left(270)
+    t.forward(20)
+    
+    t.left(270)
+    t.forward(40)
+    
+    t.left(90)
+    t.forward(65)
+
+    t.left(90)
+    t.forward(130)
+    
+    t.left(90)
+    t.forward(30)
+    
+    t.left(135)
+    t.forward(30)
+    
+    t.left(225)
+    t.forward(20)
+
+nombre()
+
+
